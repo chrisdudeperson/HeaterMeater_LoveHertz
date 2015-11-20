19297aa18d05041cf4057e258fbd56fd64d34c7b
chriswillis <chriswillis10@gmail.com>
2015-11-20 19:35:21 +0000

    Chris test commit

--- app/src/main/java/hm/heatermeater/HomeScreen.java
+++ app/src/main/java/hm/heatermeater/HomeScreen.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+//TEST
 public class HomeScreen extends AppCompatActivity {
 
     @Override
